745a6f884f95971d51b85254a17e577433a0a4f6
Ridost <liao60209@yahoo.com.tw>
2020-06-16 03:15:32 +0800

    ImageProcess_v3

--- ImageProcess/ImageProcess/Form1.Designer.cs
+++ ImageProcess/ImageProcess/Form1.Designer.cs
@@ -36,16 +36,20 @@
             this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
             this.hScrollBar2 = new System.Windows.Forms.HScrollBar();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 19);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(567, 309);
+            this.pictureBox1.Size = new System.Drawing.Size(820, 504);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
@@ -56,7 +60,8 @@
             // 
             // LoadImage
             // 
-            this.LoadImage.Location = new System.Drawing.Point(28, 402);
+            this.LoadImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.LoadImage.Location = new System.Drawing.Point(862, 304);
             this.LoadImage.Name = "LoadImage";
             this.LoadImage.Size = new System.Drawing.Size(93, 52);
             this.LoadImage.TabIndex = 1;
@@ -66,17 +71,19 @@
             // 
             // panel1
             // 
+            this.panel1.AutoSize = true;
             this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Location = new System.Drawing.Point(28, 35);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(567, 309);
+            this.panel1.Size = new System.Drawing.Size(820, 526);
             this.panel1.TabIndex = 2;
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(757, 44);
+            this.label1.Location = new System.Drawing.Point(859, 44);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(37, 15);
             this.label1.TabIndex = 8;
@@ -84,8 +91,9 @@
             // 
             // label2
             // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(757, 107);
+            this.label2.Location = new System.Drawing.Point(859, 107);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(52, 15);
             this.label2.TabIndex = 9;
@@ -93,8 +101,9 @@
             // 
             // hScrollBar1
             // 
+            this.hScrollBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.hScrollBar1.LargeChange = 1;
-            this.hScrollBar1.Location = new System.Drawing.Point(810, 44);
+            this.hScrollBar1.Location = new System.Drawing.Point(912, 44);
             this.hScrollBar1.Maximum = 510;
             this.hScrollBar1.Name = "hScrollBar1";
             this.hScrollBar1.Size = new System.Drawing.Size(161, 20);
@@ -104,8 +113,9 @@
             // 
             // hScrollBar2
             // 
+            this.hScrollBar2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.hScrollBar2.LargeChange = 1;
-            this.hScrollBar2.Location = new System.Drawing.Point(810, 102);
+            this.hScrollBar2.Location = new System.Drawing.Point(912, 102);
             this.hScrollBar2.Name = "hScrollBar2";
             this.hScrollBar2.Size = new System.Drawing.Size(161, 20);
             this.hScrollBar2.TabIndex = 102;
@@ -113,18 +123,43 @@
             // 
             // comboBox1
             // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(760, 177);
+            this.comboBox1.Location = new System.Drawing.Point(862, 177);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(168, 23);
             this.comboBox1.TabIndex = 103;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(980, 304);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 52);
+            this.button1.TabIndex = 104;
+            this.button1.Text = "Cut";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(862, 386);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 52);
+            this.button2.TabIndex = 105;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(990, 466);
+            this.ClientSize = new System.Drawing.Size(1095, 725);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.hScrollBar2);
             this.Controls.Add(this.hScrollBar1);
@@ -152,6 +187,8 @@
         private System.Windows.Forms.HScrollBar hScrollBar1;
         private System.Windows.Forms.HScrollBar hScrollBar2;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- ImageProcess/ImageProcess/Form1.cs
+++ ImageProcess/ImageProcess/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -17,7 +18,7 @@ namespace ImageProcess
         public Form1()
         {
             InitializeComponent();
-            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
         }
         public Bitmap myBitmap;
         public string TempImagePath;
@@ -34,7 +35,8 @@ namespace ImageProcess
             CurrentImagePath = of.FileName.ToString();
             CurrentImageName = of.SafeFileName;
             this.myBitmap = new Bitmap(CurrentImagePath);
-            new Bitmap(CurrentImagePath).Save("temp.png");
+            new Bitmap(CurrentImagePath).Save("temp_origin.png");
+            new Bitmap(CurrentImagePath).Save("temp_revised.png");
             // Stretches the image to fit the pictureBox.
             Bitmap myImage = this.myBitmap;//new Bitmap(fileToDisplay);
                                            //pictureBox1.ClientSize = new Size(xSize, ySize);
@@ -78,6 +80,14 @@ namespace ImageProcess
         {
             isDragging = true;
             if (isLoaded == false) return;
+            if (e.Button == MouseButtons.Right) {
+                startX = 0;
+                startY = 0;
+                endX = 0;
+                endY = 0;
+                pictureBox1.Refresh();
+                return;
+            }
             startX = e.X;
             startY = e.Y;
         }
@@ -102,7 +112,7 @@ namespace ImageProcess
             {
                 string basedir = dir.Parent.Parent.Parent.Parent.FullName;
                 string pyexePath = basedir + "\\Scripts\\Python\\contrast.py";
-                string outputPath = "temp.png";
+                string outputPath = "temp_revised.png";
                 string gamma;
                 if (hScrollBar2.Value < 50) gamma = ((float)(hScrollBar2.Value + 1) / 50).ToString();
                 else if (hScrollBar2.Value > 51) gamma = ((hScrollBar2.Value - 50) / 4).ToString();
@@ -115,7 +125,7 @@ namespace ImageProcess
                 p.StartInfo.RedirectStandardOutput = true;//輸出引數設定
                 p.StartInfo.RedirectStandardInput = true;//傳入引數設定
                 p.StartInfo.CreateNoWindow = true;
-                p.StartInfo.Arguments = pyexePath + " " + CurrentImagePath + " " + gamma + " temp.png";//引數以空格分隔，如果某個引數為空，可以傳入””
+                p.StartInfo.Arguments = pyexePath + " " + "temp_origin.png" + " " + gamma + " temp_revised.png";//引數以空格分隔，如果某個引數為空，可以傳入””
                 p.Start();
                 string output = p.StandardOutput.ReadToEnd();
                 p.WaitForExit();
@@ -123,7 +133,7 @@ namespace ImageProcess
                 p.Close();
 
                 FileStream fs;
-                fs = new FileStream("temp.png", FileMode.Open, FileAccess.Read);
+                fs = new FileStream("temp_revised.png", FileMode.Open, FileAccess.Read);
                 pictureBox1.Image = System.Drawing.Image.FromStream(fs);
                 fs.Close();
             }
@@ -135,7 +145,7 @@ namespace ImageProcess
             {
                 string basedir = dir.Parent.Parent.Parent.Parent.FullName;
                 string pyexePath = basedir + "\\Scripts\\Python\\brightness.py";
-                string outputPath = "temp.png";
+                string outputPath = "temp_revised.png";
                 string brightness = (hScrollBar1.Value-255).ToString();
 
                 Process p = new Process();
@@ -145,7 +155,7 @@ namespace ImageProcess
                 p.StartInfo.RedirectStandardOutput = true;//輸出引數設定
                 p.StartInfo.RedirectStandardInput = true;//傳入引數設定
                 p.StartInfo.CreateNoWindow = true;
-                p.StartInfo.Arguments = pyexePath + " "+CurrentImagePath+" " + brightness + " temp.png";//引數以空格分隔，如果某個引數為空，可以傳入””
+                p.StartInfo.Arguments = pyexePath + " "+"temp_origin.png"+" " + brightness + " temp_revised.png";//引數以空格分隔，如果某個引數為空，可以傳入””
                 p.Start();
                 string output = p.StandardOutput.ReadToEnd();
                 p.WaitForExit();
@@ -153,7 +163,7 @@ namespace ImageProcess
                 p.Close();
 
                 FileStream fs;
-                fs = new FileStream("temp.png", FileMode.Open, FileAccess.Read);
+                fs = new FileStream("temp_revised.png", FileMode.Open, FileAccess.Read);
                 pictureBox1.Image = System.Drawing.Image.FromStream(fs);
                 fs.Close();
             }
@@ -169,7 +179,7 @@ namespace ImageProcess
             if (isLoaded) {
                 string basedir = dir.Parent.Parent.Parent.Parent.FullName;
                 string pyexePath = basedir + "\\Scripts\\Python\\basic_func.py";
-                string outputPath = "temp.png";
+                string outputPath = "temp_origin.png";
                 Process p = new Process();
                 //p.StartInfo.FileName = pyexePath;//需要執行的檔案路徑
                 p.StartInfo.FileName = "python.exe";
@@ -177,7 +187,7 @@ namespace ImageProcess
                 p.StartInfo.RedirectStandardOutput = true;//輸出引數設定
                 p.StartInfo.RedirectStandardInput = true;//傳入引數設定
                 p.StartInfo.CreateNoWindow = true;
-                p.StartInfo.Arguments = pyexePath +" "+comboBox1.SelectedIndex.ToString()+" "+CurrentImagePath+" temp.png";//引數以空格分隔，如果某個引數為空，可以傳入””
+                p.StartInfo.Arguments = pyexePath +" "+comboBox1.SelectedIndex.ToString()+" "+"temp_origin.png"+" temp_revised.png";//引數以空格分隔，如果某個引數為空，可以傳入””
                 p.Start();
                 string output = p.StandardOutput.ReadToEnd();
                 p.WaitForExit();
@@ -185,7 +195,7 @@ namespace ImageProcess
                 p.Close();
 
                 FileStream fs;
-                fs = new FileStream("temp.png", FileMode.Open, FileAccess.Read);
+                fs = new FileStream("temp_revised.png", FileMode.Open, FileAccess.Read);
                 pictureBox1.Image = System.Drawing.Image.FromStream(fs);
                 fs.Close();
             }
@@ -194,6 +204,23 @@ namespace ImageProcess
         private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
         {
             isDragging = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Cut(this.myBitmap,startX,startY,endX-startX,endY-startY);
+            pictureBox1.Image.Save("temp_origin.png");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            FileStream fs;
+            fs = new FileStream(CurrentImagePath, FileMode.Open, FileAccess.Read);
+            pictureBox1.Image = System.Drawing.Image.FromStream(fs);
+            fs.Close();
+            new Bitmap(CurrentImagePath).Save("temp_origin.png");
+            new Bitmap(CurrentImagePath).Save("temp_revised.png");
         }
 
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
@@ -214,8 +241,8 @@ namespace ImageProcess
                 }
                 string basedir = dir.Parent.Parent.Parent.Parent.FullName;
                 string pyexePath = basedir + "\\Scripts\\Python\\brightness.py";
-                string outputPath = "temp.png";
-                string brightness = "180";
+                string outputPath = "temp_origin.png";
+                string brightness = "100";
 
                 Process p = new Process();
                 //p.StartInfo.FileName = pyexePath;//需要執行的檔案路徑
@@ -224,7 +251,7 @@ namespace ImageProcess
                 p.StartInfo.RedirectStandardOutput = true;//輸出引數設定
                 p.StartInfo.RedirectStandardInput = true;//傳入引數設定
                 p.StartInfo.CreateNoWindow = true;
-                p.StartInfo.Arguments = pyexePath + " " + CurrentImagePath + " " + brightness + " temp.png "+startX+" "+startY+" "+(endX-startX)+" "+(endY-startY);//引數以空格分隔，如果某個引數為空，可以傳入””
+                p.StartInfo.Arguments = pyexePath + " " + "temp_origin.png" + " " + brightness + " temp_revised.png "+startX+" "+startY+" "+(endY-startY) +" "+ (endX - startX);//引數以空格分隔，如果某個引數為空，可以傳入””
                 p.Start();
                 string output = p.StandardOutput.ReadToEnd();
                 p.WaitForExit();
@@ -232,11 +259,45 @@ namespace ImageProcess
                 p.Close();
 
                 FileStream fs;
-                fs = new FileStream("temp.png", FileMode.Open, FileAccess.Read);
+                fs = new FileStream("temp_revised.png", FileMode.Open, FileAccess.Read);
                 pictureBox1.Image = System.Drawing.Image.FromStream(fs);
                 fs.Close();
                 pictureBox1.Refresh();
             }
         }
+        public static Bitmap Cut(Bitmap b, int StartX, int StartY, int iWidth, int iHeight)
+        {
+            if (b == null)
+            {
+                return null;
+            }
+            int w = b.Width;
+            int h = b.Height;
+            if (StartX >= w || StartY >= h)
+            {
+                return null;
+            }
+            if (StartX + iWidth > w)
+            {
+                iWidth = w - StartX;
+            }
+            if (StartY + iHeight > h)
+            {
+                iHeight = h - StartY;
+            }
+            try
+            {
+                Bitmap bmpOut = new Bitmap(iWidth, iHeight, PixelFormat.Format24bppRgb);
+                Graphics g = Graphics.FromImage(bmpOut);
+                g.DrawImage(b, new Rectangle(0, 0, iWidth, iHeight), new Rectangle(StartX, StartY, iWidth, iHeight), GraphicsUnit.Pixel);
+                g.Dispose();
+                return bmpOut;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    
     }
 }
