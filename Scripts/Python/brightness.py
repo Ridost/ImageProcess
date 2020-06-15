bfd4c90b4e921d4c9609f90367f86dd642a0a7a8
soimportant <soimportant0318@gmail.com>
2020-06-15 19:25:11 +0800

    Add readme.md

--- Scripts/Python/brightness.py
+++ Scripts/Python/brightness.py
@@ -1,1 +1,15 @@
-import 
+import argparse
+from PIL import Image
+
+
+def GammaCorrection(pixel, gamma=1.0):
+    pass
+
+
+def main():
+    image = Image.open()
+
+
+if __name__ == "__main__":
+    main()
+
